8ea72cfd4fb3a474fab1349c97c80f15a77624bc
Lucas Copque <copque.faculdade@gmail.com>
2019-02-05 12:30:39 -0200

    start projeto

---
+++ src/main/java/br/com/incentivados/IncentivadosApplication.java
@@ -0,0 +1,14 @@
+package br.com.incentivados;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IncentivadosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IncentivadosApplication.class, args);
+	}
+
+}
+
---
+++ src/main/webapp/WEB-INF/views/index.jsp
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+
+<html lang="pt-br">
+<head>
+	<title>Spring Boot JSP example</title>
+	<link rel="stylesheet" type="text/css" href="/style.css">
+</head>
+<body>
+	<h1>Hello World</h1>
+</body>
+	<script type="text/javascript" href="/script.js"></script>
+</html>
